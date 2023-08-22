5c8913efd9b08375f24f730429897aaf6dda5786
Ankur Semle <semleankur@gmail.com>
2023-08-22 16:35:22 +0530

    checkpoint#2 adding db

--- packages/ui/components/Signup.tsx
+++ packages/ui/components/Signup.tsx
@@ -1,8 +1,60 @@
+import { Typography, Card, TextField, Button } from "@mui/material";
+import { useState } from "react";
 
-export function Signup(){
-    return <div>
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="password" />
-<button>Signup</button>
+export function Signup(props: {
+  onClick: (username: string, password: string) => void;
+}) {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  return (
+    <div>
+      <div
+        style={{
+          paddingTop: 150,
+          marginBottom: 10,
+          display: "flex",
+          justifyContent: "center"
+        }}
+      >
+        <Typography variant={"h6"}>
+          Welcome to Coursera. Sign up below
+        </Typography>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Card variant={"outlined"} style={{ width: 400, padding: 20 }}>
+          <TextField
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+            fullWidth={true}
+            label="Email"
+            variant="outlined"
+          />
+          <br />
+          <br />
+          <TextField
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            fullWidth={true}
+            label="Password"
+            variant="outlined"
+            type={"password"}
+          />
+          <br />
+          <br />
+          <Button
+            size={"large"}
+            variant="contained"
+            onClick={async () => {
+              props.onClick(email, password);
+            }}
+          >
+            Signup
+          </Button>
+        </Card>
+      </div>
     </div>
+  );
 }
